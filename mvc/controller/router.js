4fa6189433406dcea0f5f5847dccc657eb4996a7
Mendes Lopes Ermelindo <ermelindo.mendes@gmail.com>
2023-10-09 13:48:45 +0200

    exo mvc

--- mvc/controller/router.js
+++ mvc/controller/router.js
@@ -3,7 +3,19 @@ const express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {username: "John Doe"});
-})
+    const username = req.session.user;
+    if (username) {
+        res.render('index', { username });
+    } else {
+        res.render('form'); 
+    }
+});
+
+
+router.post('/', (req, res) => {
+    const { username } = req.body;
+    req.session.user = username; 
+    res.redirect('/'); 
+});
 
 module.exports = router;
--- mvc/index.js
+++ mvc/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./controller/router');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 let app = express();
 
 // dire a node que l'on utilise ejs comme moteur de templates
@@ -10,6 +12,15 @@ app.set('views', path.join(__dirname,'views'));
 
 //let routes = require('./controller/routes')
 //app.use('/', routes)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'votre_secret',
+    resave: false,
+    saveUninitialized: true,
+  }));
+  
 
 app.use('/', routes)
 
